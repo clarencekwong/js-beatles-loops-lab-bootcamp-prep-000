6a3436de4e01b9480218101fcf6b11f4633c7163
Clarence Kwong <clarencekwong7@gmail.com>
2018-03-15 19:38:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay (ar1,ar2) {
   var ar3 = []
   for (var i = 0; i < ar1.length + 1; i++) {
-     ar3.push(`${ar1[i]}`.' plays '.`${ar2[i]}`)
+     ar3.push(`${ar1[i]}` + ' plays ' + `${ar2[i]}`)
   }
+  return ar3
 }
